146bfabc946a5f3df46e8ffda37c3d83c06275cd
Kristijan <kristijanjovik@gmail.com>
2026-02-10 16:31:33 +0100

    Fixed timer modal

--- timer-app/src/components/ResultModal.jsx
+++ timer-app/src/components/ResultModal.jsx
@@ -24,7 +24,7 @@ export default function ResultModal({
     <dialog ref={dialog} className="result-modal" onClose={onReset}>
       {noTimeLeft ? <h2>You lost</h2> : <h2>Your score: {score}</h2>}
       <p>
-        The target time was <strong>{formattedResultTime} seconds.</strong>
+        The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
         You stopped the timer with{" "}
